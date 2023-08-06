7fd397877d07580af24199bc58e004f9214b9e4a
Alessandro Salzo <58805919+Tarkbyte@users.noreply.github.com>
2023-08-07 01:04:39 +0200

    Feature: Connection to the api, and show todos in the template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <template>
-  <p class="text-red-500">Hello World</p>
+  <TodoItem v-for="todo in todoList" :key="todo.id" :todo="todo" />
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import { apiClient } from "./api/apiClient";
+import TodoItem from "./components/TodoItem.vue";
+
+const todoList = ref([]);
+const isLoading = ref(false);
+
+const getTodos = async () => {
+  try {
+    isLoading.value = true;
+    const response = await apiClient.get("/todos");
+
+    todoList.value = response.data;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(() => {
+  getTodos();
+});
+</script>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>{{ todo.title }}</p>
+</template>
+
+<script setup>
+const props = defineProps({
+  todo: {
+    type: Object,
+    default: () => {},
+  },
+});
+</script>
